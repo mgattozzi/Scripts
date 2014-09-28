128d29c29d900249f569f135aaa4903374fc3bcb
Michael Gattozzi <mgattozzi@gmail.com>
2014-09-28 05:02:56 -0400

    Added STDError redirection

--- Arch Linux Scripts/sign-key.sh
+++ Arch Linux Scripts/sign-key.sh
@@ -1,20 +1,19 @@
 #!/bin/zsh
 exit_code=0
 if [ $(id -u) != "0" ]; then
-    echo "You must be the superuser to run this script"
+    echo "You must be the superuser to run this script" >&2
 	exit_code=1
 	exit $exit_code
 elif [ $# -ge "1" ]; then
 	while [ $# != "0" ]; do
-		
 		if pacman-key -r $1 && pacman-key -f $1; then
 			pacman-key --lsign-key $1
 		else
 			if [ $# -gt "1" ]; then
-				echo "Invalid key entered, moving on to next key"	
+				echo "Invalid key entered, moving on to next key" >&2
 				exit_code=1
 			else
-				echo "Invalid key, please enter a valid key"
+				echo "Invalid key, please enter a valid key" >&2
 				exit_code=1
 				exit $exit_code
 			fi
@@ -27,7 +26,7 @@ elif [ $# -ge "1" ]; then
 		fi
 	done
 fi
-echo "Insert keys to sign"
+echo "Insert keys to sign" >&2
 exit_code=0
 exit $exit_code
 
